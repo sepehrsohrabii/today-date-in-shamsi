61f09990c8a7fe27c9d1fa94f949ea643c2da6bc
Sepehr Sohrabi <sepehr0sohrabi@gmail.com>
2022-10-09 21:52:50 +0330

    final push

---
+++ exercise_two.js
@@ -0,0 +1,7 @@
+// Using jalali-date npm package
+// Installation: npm install jalali-date
+
+const JDate = require("jalali-date");
+const jdate = new JDate(); // => default to today
+
+console.log(jdate);
